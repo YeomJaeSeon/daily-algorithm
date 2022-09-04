150f1da217dab99a02e075457b52f4f2314b25fb
min sun Yeom <alextep@minui-MacBookAir.local>
2022-09-04 19:27:35 +0900

    week 2 yeom min seon

---
+++ YeomMinSeon/EliminationbyGrouping.java
@@ -0,0 +1,28 @@
+package JoyStick.algoday1;
+import java.util.*;
+
+public class EliminationbyGrouping {
+
+
+        public int solution(String s) {
+            Stack<String> stack = new Stack<>();
+
+            for(int i=0; i<s.length(); i++){
+                if(stack.isEmpty()){
+                    stack.push(String.valueOf(s.charAt(i)));
+                }else{
+                    String lastVal = stack.peek();
+                    String currVal = String.valueOf(s.charAt(i));
+                    if(!lastVal.equals(currVal)){
+                        stack.push(currVal);
+                    }else{
+                        stack.pop();
+                    }
+                }
+            }
+
+            return stack.size() == 0 ? 1 : 0;
+        }
+    }
+
+
