e58295eb6a165fc95613009577bf3065c9436c0f
evan <evan@baekdutech.com>
2022-09-11 19:28:17 +0900

    추석트래픽 성공!

--- KimTaeHee/September/11/[1차]추석트래픽.js
+++ KimTaeHee/September/11/[1차]추석트래픽.js
@@ -1,7 +1,8 @@
 function solution(lines) {
   var answer = 0;
-  const answerMap = {};
   let max = -1;
+  const answerMap = {};
+  const time = [];
   for (let i = 0; i < lines.length; i++) {
     const splited = lines[i].split(' ');
     const s = splited[1];
@@ -15,14 +16,18 @@ function solution(lines) {
       1000 * (3600 * hour + 60 * minute + second * 1) + milliSeconds * 1;
     const tMilliSecond = t.substring(0, t.length - 1) * 1000;
     const startTime = sum - tMilliSecond + 1;
-    for (let j = startTime; j <= sum; j++) {
-      if (Object.keys(answerMap).includes(j)) {
-        answerMap[j] += 1;
-      } else {
-        answerMap[j] = 1;
-      }
-      max = Math.max(max, answerMap[j]);
+    time.push(['start', startTime]);
+    time.push(['end', sum + 999]);
+  }
+  time.sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : -1));
+  for (let t of time) {
+    if (t[0] === 'start') {
+      answer += 1;
+    } else {
+      answer -= 1;
     }
+    console.log(answer);
+    max = Math.max(answer, max);
   }
   return max;
 }
